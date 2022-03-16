43c77a876f69f9da535d56d9705b58d5aad0f45a
PC55 <laryea.enl99@gmial.com>
2022-03-16 10:29:21 +0000

    update

--- src/components/TaskDetais.js
+++ src/components/TaskDetais.js
@@ -6,31 +6,24 @@ const TaskDetais = () => {
 
     const [loading, setLoading] = useState(true)
     const [task, setTask] = useState({})
-    const [error, setError] = useState(null)
+    // const [error, setError] = useState(null)
 
     const params = useParams()
     const navigate = useNavigate()
     const { pathname } = useLocation()
+    const location = useLocation();
+    const tasks = location.state;
+
     useEffect(() => {
-        const fetchTask = async () => {
-            const res = await fetch(`http://localhost:5000/tasks/${params.id}`)
-            const data = await res.json()
-
-            if (res.status === 404) {
-                setError('Task not found')
-                navigate('/')
-            }
-
-            setTask(data)
+        const task = tasks.find((task) => task.id == params.id)
+        if (task) {
+            setTask(task)
             setLoading(false)
+            return
         }
+        navigate('/')
+    }, [])
 
-        fetchTask()
-    })
-
-    if (error) {
-        // return <Navigate to='/' />
-    }
 
     return loading ? (
         <h3>Loading...</h3>
